c1f349c9c5f71415d9809873c9e2919a621c01af
root <root@DESKTOP-5BCIDIQ>
2024-12-26 20:07:39 +0800

    Remove setting of cargo dir

--- Readme.md
+++ Readme.md
@@ -1,6 +1,12 @@
 # Tmux配置文件
-执行install.sh来将Tmux配置文件导入ubuntu系统：
+首先将文件拷贝到根目录下：
 
 ```bash
-bash install.sh
+cd ~ && git clone https://github.com/Sahala08/dotfiles.git
+```
+
+然后执行install.sh来将Tmux配置文件导入ubuntu系统：
+
+```bash
+cd dotfiles && bash install.sh
 ```
--- install.sh
+++ install.sh
@@ -2,9 +2,6 @@
 
 DOTFILES_DIR="$HOME/dotfiles"
 
-grep -q '^\.\ "$HOME/.cargo/env"$' ~/.bashrc || echo '. "$HOME/.cargo/env"' >> "$HOME/.bashrc"
-grep -q 'export PATH="\$HOME/.cargo/bin:\$PATH"' ~/.bashrc || echo 'export PATH="$HOME/.cargo/bin:$PATH"' >> "$HOME/.bashrc"
-
 ln -sf "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
 
 source "$HOME/.bashrc"
